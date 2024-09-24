40e3f31bfb54387b71ce4fbdaccf17f610fdc049
danielz0102 <danyzglez@hotmail.com>
2024-09-23 22:08:16 -0600

    Pass test 1: 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    const difference = end - start
+    let numbers = [start]
+    let result = 0
 
+    for (let i = 1; i < difference; i++) {
+        numbers.push(start + i)
+    }
+
+    numbers.push(end)
+
+    numbers.forEach(n => {
+        result += n
+    })
+
+    return result
 };
 
 // Do not edit below this line
