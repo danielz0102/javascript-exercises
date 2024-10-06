8f878e5d271d591731a664a73e08cf4791fed56b
danielz0102 <danyzglez@hotmail.com>
2024-10-06 00:22:40 -0600

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const peopleAges = people.map(person => {
+        if (person.yearOfDeath) {
+            return {
+                name: person.name,
+                age: person.yearOfDeath - person.yearOfBirth,
+            }
+        }
 
+        const CURRENT_YEAR = new Date().getFullYear()
+        return {
+            name: person.name,
+            age: CURRENT_YEAR - person.yearOfBirth,
+        }
+    })
+
+    return peopleAges.reduce((oldestPerson, currentPerson) => currentPerson.age > oldestPerson.age ? currentPerson : oldestPerson)
 };
 
 // Do not edit below this line
