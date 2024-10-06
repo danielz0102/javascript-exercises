d7c89962eaa73883f4c2e52d7fc92cad7cf16415
danielz0102 <danyzglez@hotmail.com>
2024-10-05 23:24:00 -0600

    Solve exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    const member = typeof arg !== 'number' ? parseInt(arg) : arg
 
+    if (member === 0) return 0
+    if (member < 0) return 'OOPS'
+
+    const fibonacci = [1, 1]
+    for (let i = 2; i <= member; i++) {
+        fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2])
+    }
+
+    return fibonacci[member - 1]
 };
 
 // Do not edit below this line
